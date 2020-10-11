a26f7a23e282aa9754a68a63fd6ee1c964643358
Ajayraj Singh <shiningcoders@gmail.com>
2020-10-11 17:06:22 +0530

    Defined Color Theme

---
+++ cusafe/lib/Theme/colorTheme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static final Color red = Color(0xffFF4757);
+  static final Color yellow = Color(0xffFFC700);
+  static final Color blue = Color(0xff00D1FF);
+  static final Color purple = Color(0xff8F03FD);
+  static final Color lightGrey = Color(0xffF6F6F6);
+  static final Color midGrey = Color(0xff9A9A9A);
+  static final Color darkGrey = Color(0xff585858);
+  static final Color white = Color(0xffffffff);
+  static final Color black = Color(0xff000000);
+}
